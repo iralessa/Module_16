7ec6f1322791d72df014b8c9d8ebe1d196379b95
iralessa <166923860+iralessa@users.noreply.github.com>
2024-09-23 23:37:56 +0300

    Update Module_16_3.py

--- Module_16_3.py
+++ Module_16_3.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Path
 from typing import Annotated
-
 app = FastAPI()
-# Инициализация словаря с пользователями
+
 users = {'1': 'Имя: Example, возраст: 18'}
 
 # 1. GET запрос, возвращает всех пользователей
@@ -12,16 +11,21 @@ async def get_users()-> dict:
 
 # 2. POST запрос для добавления нового пользователя
 @app.post("/user/{username}/{age}")
-async def add_user(username: str, age: int)-> str:
+async def add_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+    age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)])-> str:
     new_id = str(max(map(int, users.keys())) + 1)
     users[new_id] = f"Имя: {username}, возраст: {age}"
     return f"User {new_id} is registered"
 
 # 3. PUT запрос для обновления пользователя
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: str, username: str, age: int)-> str:
-    if user_id in users:
-        users[user_id] = f"Имя: {username}, возраст: {age}"
+async def update_user(
+    user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example=1)],
+    username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username", example="UrbanUser")],
+    age: Annotated[int, Path(ge=18, le=120, description="Enter age", example=24)]
+) -> str:
+    if str(user_id) in users:
+        users[str(user_id)] = f"Имя: {username}, возраст: {age}"
         return f"User {user_id} has been updated"
     return f"User {user_id} not found"
 
@@ -32,4 +36,5 @@ async def delete_user(user_id: str)-> str:
         del users[user_id]
         return f"User {user_id} has been deleted"
     return f"User {user_id} not found"
+
 
